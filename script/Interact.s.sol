9da64052924e43d1bc13856d69150b274419aa0f
ervand7 <ervand7@mail.ru>
2024-11-03 21:13:27 +0100

    initial commit

---
+++ script/Interact.s.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { Script, console } from "forge-std/Script.sol";
+import { DevOpsTools } from "foundry-devops/src/DevOpsTools.sol";
+import { MerkleAirdrop } from "../src/MerkleAirdrop.sol";
+
+contract ClaimAirdrop is Script {
+    address private constant CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; // anvil
+    uint256 private constant AMOUNT_TO_COLLECT = (25 * 1e18);
+
+    bytes32 private constant PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 private constant PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] private proof = [PROOF_ONE, PROOF_TWO];
+
+    bytes private SIGNATURE =
+        hex"04209f8dfd0ef06724e83d623207ba8c33b6690e08772f8887a4eaf9a66b9182188938adea374fa542ad5ddde24bdc981f5e26a628e65fb425a68db8a938f6761c";
+
+    error __ClaimAirdropScript__InvalidSignatureLength();
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MerkleAirdrop", block.chainid);
+        claimAirdrop(mostRecentlyDeployed);
+    }
+
+    function claimAirdrop(address airdrop) public {
+        vm.startBroadcast();
+
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
+        console.log("Claiming Airdrop");
+        MerkleAirdrop(airdrop).claim(CLAIMING_ADDRESS, AMOUNT_TO_COLLECT, proof, v, r, s);
+        vm.stopBroadcast();
+        console.log("Claimed Airdrop");
+    }
+
+    function splitSignature(bytes memory sig) public pure returns (uint8 v, bytes32 r, bytes32 s) {
+        // Check if the provided signature has the correct length (65 bytes).
+        // ECDSA signatures are always 65 bytes: 32 bytes for `r`, 32 bytes for `s`, and 1 byte for `v`.
+        if (sig.length != 65) {
+            revert __ClaimAirdropScript__InvalidSignatureLength();
+        }
+
+        // Use assembly to manually extract the `r`, `s`, and `v` values from the signature.
+        // Assembly allows direct access to memory, which is used here to pull out the relevant components.
+        assembly {
+            // The first 32 bytes of the signature (after the first 32-byte word) are loaded into `r`.
+            r := mload(add(sig, 32))
+            // The second 32 bytes (next word in memory) are loaded into `s`.
+            s := mload(add(sig, 64))
+            // The last byte (offset by 96 bytes) is loaded into `v` (this is the recovery id).
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+}
---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import { IERC20, SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import { SignatureChecker } from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+/**
+ * @title Merkle Airdrop - Airdrop tokens to users who can prove they are in a merkle tree
+ * @author Ervand Agadzhanyan
+ */
+contract MerkleAirdrop is EIP712 {
+    using ECDSA for bytes32;
+    using SafeERC20 for IERC20; // Prevent sending tokens to recipients who can’t receive
+
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
+    error MerkleAirdrop__InvalidSignature();
+
+    IERC20 private immutable i_airdropToken;
+    bytes32 private immutable i_merkleRoot;
+    mapping(address => bool) private s_hasClaimed;
+
+    // MESSAGE_TYPEHASH is a hash of the message structure description. It is used to ensure
+    // integrity and security when signing messages in accordance with the EIP-712 standard.
+    bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account,uint256 amount)");
+
+    struct AirdropClaim {
+        address account;
+        uint256 amount;
+    }
+
+    event Claimed(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) EIP712("Merkle Airdrop", "1.0.0") {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(
+        address account, // The address of the account attempting to claim the airdrop.
+        uint256 amount, // The amount of tokens being claimed by the account.
+        bytes32[] calldata merkleProof, // A proof (array of hashes) used to verify the
+        // account's presence in the Merkle tree.
+        uint8 v, // Part of the signature (EIP-712) for verifying the claim, representing the recovery id.
+        bytes32 r, // Part of the signature (EIP-712), representing the first 32 bytes of the signature.
+        bytes32 s // Part of the signature (EIP-712), representing the second 32 bytes of the signature.
+    )
+        external
+    {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+
+        if (!_isValidSignature(account, getMessageHash(account, amount), v, r, s)) {
+            revert MerkleAirdrop__InvalidSignature();
+        }
+
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+
+        s_hasClaimed[account] = true;
+        emit Claimed(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
+
+    // message we expect to have been signed
+    function getMessageHash(address account, uint256 amount) public view returns (bytes32) {
+        return _hashTypedDataV4(
+            keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({ account: account, amount: amount })))
+        );
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
+
+    // verify whether the recovered signer is the expected signer/the account to airdrop tokens for
+    function _isValidSignature(
+        address signer,
+        bytes32 digest,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    )
+        internal
+        pure
+        returns (bool)
+    {
+        (
+            address actualSigner,
+            /*ECDSA.RecoverError recoverError*/
+            ,
+            /*bytes32 signatureLength*/
+        ) = ECDSA.tryRecover(digest, _v, _r, _s);
+        return (actualSigner == signer);
+    }
+}
